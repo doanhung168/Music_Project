f8689c25ad07599145ff8e6e63c6026bf23533ad
hungcutephomaiqueconbocuoi <hung0868358175@gmail.com>
2022-08-10 11:06:54 +0700

    add setting fragment

---
+++ app/src/main/java/com/doanhung/musicproject/view/main_activity/setting_fragment/SettingFragment.java
@@ -0,0 +1,100 @@
+package com.doanhung.musicproject.view.main_activity.setting_fragment;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.doanhung.musicproject.R;
+import com.doanhung.musicproject.data.model.app_system_model.DeviceItem;
+import com.doanhung.musicproject.data.repository.AppSystemRepository;
+import com.doanhung.musicproject.databinding.FragmentSettingBinding;
+import com.doanhung.musicproject.di.AppModule;
+import com.doanhung.musicproject.view.BaseFragment;
+import com.doanhung.musicproject.view.common_adapter.DeviceItemAdapter;
+import com.doanhung.musicproject.view.main_activity.OnOpenNavigationViewListener;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
+public class SettingFragment extends BaseFragment<FragmentSettingBinding>
+        implements DeviceItemAdapter.OnClickDeviceItemListener {
+
+    private OnOpenNavigationViewListener mOnOpenNavViewListener;
+
+    @Inject
+    @AppModule.DeviceItemAdapterWithoutDividerAnnotation
+    DeviceItemAdapter mDeviceItemAdapter;
+
+    @Inject
+    AppSystemRepository mAppSystemRepository;
+    private SettingViewModel mSettingViewModel;
+
+    @Override
+    public int getFragmentView() {
+        return R.layout.fragment_setting;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initViewModels();
+        setupToolbar();
+        setupRcvSettings();
+        listenLoadingSettingDataEvens();
+    }
+
+
+    private void initViewModels() {
+        mSettingViewModel =
+                new ViewModelProvider(this,
+                        new SettingViewModel.SettingViewModelFactory(mAppSystemRepository))
+                        .get(SettingViewModel.class);
+    }
+
+    private void setupToolbar() {
+        mOnOpenNavViewListener = (OnOpenNavigationViewListener) requireActivity();
+        mBinding.toolbar.setNavigationOnClickListener(v -> mOnOpenNavViewListener.openOrCloseNavView());
+    }
+
+    private void setupRcvSettings() {
+        mDeviceItemAdapter.setOnClickItemListener(this);
+        mBinding.rcvSettings.setAdapter(mDeviceItemAdapter);
+
+        if (mSettingViewModel.checkHasNoSettingData()) {
+            mSettingViewModel.loadSettingData();
+        }
+        mSettingViewModel.mSettingItems.observe(getViewLifecycleOwner(), items -> {
+            if (items != null) mDeviceItemAdapter.submitList(items);
+        });
+    }
+
+    private void listenLoadingSettingDataEvens() {
+        mSettingViewModel.mEvent.observe(getViewLifecycleOwner(), event -> {
+            switch (event) {
+                case LOADING_SETTING_FRAGMENT_DATA_EVENT:
+                    // may be show loading progress bar
+                    break;
+                case LOADING_SETTING_FRAGMENT_DATA_FAILURE_EVENT:
+                    Toast.makeText(requireContext(), event.getException().getMessage(), Toast.LENGTH_SHORT)
+                            .show();
+                    break;
+            }
+        });
+    }
+
+    @Override
+    public void onClickDeviceItem(DeviceItem deviceItem) {
+        Toast.makeText(requireContext(), deviceItem.getName(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
---
+++ app/src/main/java/com/doanhung/musicproject/view/main_activity/setting_fragment/SettingViewModel.java
@@ -0,0 +1,68 @@
+package com.doanhung.musicproject.view.main_activity.setting_fragment;
+
+import static com.doanhung.musicproject.util.event.Event.LOADING_SETTING_FRAGMENT_DATA_EVENT;
+import static com.doanhung.musicproject.util.event.Event.LOADING_SETTING_FRAGMENT_DATA_FAILURE_EVENT;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.doanhung.musicproject.data.model.app_system_model.DeviceItem;
+import com.doanhung.musicproject.data.repository.AppSystemRepository;
+import com.doanhung.musicproject.util.Result;
+import com.doanhung.musicproject.util.event.Event;
+import com.doanhung.musicproject.util.event.SingleLiveEvent;
+
+import java.util.List;
+
+public class SettingViewModel extends ViewModel {
+    private final AppSystemRepository mAppSystemRepository;
+
+    private final SingleLiveEvent<Event> _mEvent = new SingleLiveEvent<>();
+    public final SingleLiveEvent<Event> mEvent = _mEvent;
+
+    private final MutableLiveData<List<DeviceItem>> _mSettingItems = new MutableLiveData<>();
+    public final LiveData<List<DeviceItem>> mSettingItems = _mSettingItems;
+
+    public SettingViewModel(AppSystemRepository appSystemRepository) {
+        this.mAppSystemRepository = appSystemRepository;
+    }
+
+    public void loadSettingData() {
+        _mEvent.setValue(LOADING_SETTING_FRAGMENT_DATA_EVENT);
+        mAppSystemRepository.loadDataForSettingFragment(result -> {
+            if (result instanceof Result.Success) {
+                _mSettingItems.postValue(((Result.Success<List<DeviceItem>>) result).data);
+            } else {
+                LOADING_SETTING_FRAGMENT_DATA_FAILURE_EVENT
+                        .setException(((Result.Error<List<DeviceItem>>) result).exception);
+                _mEvent.postValue(LOADING_SETTING_FRAGMENT_DATA_FAILURE_EVENT);
+            }
+        });
+    }
+
+    public boolean checkHasNoSettingData() {
+        return _mSettingItems.getValue() == null;
+    }
+
+    public static class SettingViewModelFactory implements ViewModelProvider.Factory {
+        private final AppSystemRepository mAppSystemRepository;
+
+        public SettingViewModelFactory(AppSystemRepository appSystemRepository) {
+            mAppSystemRepository = appSystemRepository;
+        }
+
+        @SuppressWarnings("unchecked")
+        @NonNull
+        @Override
+        public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+            if (modelClass.isAssignableFrom(SettingViewModel.class)) {
+                return (T) new SettingViewModel(mAppSystemRepository);
+            }
+            throw new IllegalArgumentException("Un construct viewModel");
+        }
+    }
+
+}
