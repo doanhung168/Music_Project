c0a9b392682d017d1e88b126650fa35d82ecf768
hungcutephomaiqueconbocuoi <hung0868358175@gmail.com>
2022-08-10 10:53:21 +0700

    add di

--- app/src/main/java/com/doanhung/musicproject/di/AppModule.java
+++ app/src/main/java/com/doanhung/musicproject/di/AppModule.java
@@ -1,27 +1,50 @@
 package com.doanhung.musicproject.di;
 
+import android.content.Context;
+
 import androidx.annotation.NonNull;
 
-import com.doanhung.musicproject.data.model.DeviceItem;
-import com.doanhung.musicproject.data.model.Song;
-import com.doanhung.musicproject.view.adapter.DeviceItemAdapter;
-import com.doanhung.musicproject.view.adapter.HotRecommendedAdapter;
+import com.doanhung.musicproject.data.model.app_system_model.DeviceItem;
+import com.doanhung.musicproject.data.model.app_system_model.DeviceSong;
+import com.doanhung.musicproject.data.model.data_model.PlayList;
+import com.doanhung.musicproject.data.model.data_model.Song;
+import com.doanhung.musicproject.service.MusicServiceController;
+import com.doanhung.musicproject.view.common_adapter.DeviceItemAdapter;
+import com.doanhung.musicproject.view.main_activity.home_fragment.adapter.HotRecommendedAdapter;
+import com.doanhung.musicproject.view.main_activity.home_fragment.adapter.PlayListAdapter;
+import com.doanhung.musicproject.view.main_activity.song_fragment.all_song_fragment.AllSongAdapter;
 
 import org.jetbrains.annotations.Contract;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
+import javax.inject.Qualifier;
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
 import dagger.hilt.InstallIn;
+import dagger.hilt.android.qualifiers.ApplicationContext;
+import dagger.hilt.android.scopes.ActivityScoped;
 import dagger.hilt.components.SingletonComponent;
 
 @Module
 @InstallIn(SingletonComponent.class)
 public class AppModule {
+
+    @Qualifier
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface DeviceItemAdapterWithoutDividerAnnotation {
+    }
+
+    @Qualifier
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface DeviceItemAdapterAnnotation {
+    }
 
     @Singleton
     @Provides
@@ -29,18 +52,47 @@ public class AppModule {
         return Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
     }
 
-    @NonNull
-    @Contract(" -> new")
     @Provides
-    public static DeviceItemAdapter provideDeviceItemAdapter() {
-        return new DeviceItemAdapter(DeviceItem.DIFF_CALLBACK);
+    public static ScheduledExecutorService provideScheduleExecutor() {
+        return Executors.newSingleThreadScheduledExecutor();
     }
 
-    @NonNull
-    @Contract(" -> new")
+
+    @DeviceItemAdapterAnnotation
+    @Provides
+    public static DeviceItemAdapter provideDeviceItemAdapter() {
+        return new DeviceItemAdapter(DeviceItem.DIFF_CALLBACK, true);
+    }
+
+    @DeviceItemAdapterWithoutDividerAnnotation
+    @Provides
+    public static DeviceItemAdapter provideDeviceItemAdapterWithoutLastDivider() {
+        return new DeviceItemAdapter(DeviceItem.DIFF_CALLBACK, false);
+    }
+
+
     @Provides
     public static HotRecommendedAdapter provideHotRecommendedAdapter() {
         return new HotRecommendedAdapter(Song.DIFF_CALLBACK);
     }
+
+    @Provides
+    public static PlayListAdapter providePlayListAdapter() {
+        return new PlayListAdapter(PlayList.DIFF_CALLBACK);
+    }
+
+
+    @Provides
+    public static AllSongAdapter provideSongAdapter() {
+        return new AllSongAdapter(DeviceSong.DIFF_CALLBACK);
+    }
+
+
+    @Singleton
+    @Provides
+    public static MusicServiceController provideMusicController(@ApplicationContext Context context) {
+        return new MusicServiceController(context);
+    }
+
 
 }
