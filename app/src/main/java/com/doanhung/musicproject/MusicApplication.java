bddc6bd1f01586e7855775cfb3a21b39ae8678a2
hungcutephomaiqueconbocuoi <hung0868358175@gmail.com>
2022-08-10 10:40:29 +0700

    add application class

--- app/src/main/java/com/doanhung/musicproject/MusicApplication.java
+++ app/src/main/java/com/doanhung/musicproject/MusicApplication.java
@@ -1,13 +1,33 @@
 package com.doanhung.musicproject;
 
-import android.app.Application;
+import static com.doanhung.musicproject.util.Constraint.MUSIC_NOTIFICATION_CHANNEL_ID;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 
 import dagger.hilt.android.HiltAndroidApp;
 
 @HiltAndroidApp
 public class MusicApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        createMusicNotificationChanel();
+    }
+
+    private void createMusicNotificationChanel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(MUSIC_NOTIFICATION_CHANNEL_ID,
+                    "Music Notification",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+
+            notificationChannel.setSound(null, null);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+    }
 
 }
